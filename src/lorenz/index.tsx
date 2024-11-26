4caa10e93611664fc7fe3b6ec4eeb02aa68a2684
Gaiwan1 <normalny_uzytkownik@proton.me>
2024-11-26 15:21:14 +0100

    feat: 2 lorenz attractors
    Signed-off-by: Gaiwan1 <normalny_uzytkownik@proton.me>

--- src/lorenz/index.tsx
+++ src/lorenz/index.tsx
@@ -21,10 +21,27 @@ const sketch = (p: p5) => {
   let σ: number;
   let ρ: number;
   let β: number;
-  let x = p.random(-30, 30);
-  let y = p.random(-30, 30);
-  let z = p.random(-30, 30);
-  let vectors: p5.Vector[] = [];
+  let σ2: number;
+  let ρ2: number;
+  let β2: number;
+  // let x = p.random(-0.5, 0.5);
+  // let y = p.random(-0.5, 0.5);
+  // let z = p.random(-0.5, 0.5);
+  //
+  // let x2 = p.random(-0.5, 0.5);
+  // let y2 = p.random(-0.5, 0.5);
+  // let z2 = p.random(-0.5, 0.5);
+
+  let x = 1;
+  let y = 1;
+  let z = 1;
+
+  let x2 = 1.00001;
+  let y2 = 1;
+  let z2 = 1;
+
+  let lorenz: p5.Vector[] = [];
+  let lorenz2: p5.Vector[] = [];
 
   // function lorenzPoints(x: number, y: number, z: number): number[] {
   //   const dx = σ * (y - x) * deltaTime;
@@ -46,6 +63,10 @@ const sketch = (p: p5) => {
     ρ = 28;
     β = 8.0 / 3;
 
+    σ2 = 10;
+    ρ2 = 28;
+    β2 = 8.0 / 3;
+
     // σ = p.random(-100, 10);
     // ρ = p.random(-100, 10);
     // β = p.random(-100, 10) / p.random(-100, 100);
@@ -63,28 +84,41 @@ const sketch = (p: p5) => {
     const dx = σ * (y - x) * deltaTime;
     const dy = (x * (ρ - z) - y) * deltaTime;
     const dz = (x * y - β * z) * deltaTime;
+
+    const dx2 = σ2 * (y2 - x2) * deltaTime;
+    const dy2 = (x2 * (ρ2 - z2) - y2) * deltaTime;
+    const dz2 = (x2 * y2 - β2 * z2) * deltaTime;
+
     x = x + dx;
     y = y + dy;
     z = z + dz;
+
+    x2 = x2 + dx2;
+    y2 = y2 + dy2;
+    z2 = z2 + dz2;
     p.translate(p.width / 2, p.height / 2);
     p.strokeWeight(0.2);
     p.scale(15);
 
-    // p.point(x, y);
-    vectors.push(p.createVector(x, y));
+    lorenz.push(p.createVector(x, y));
+    lorenz2.push(p.createVector(x2, y2));
 
+    p.fill(0, 0, 0, 0);
     p.beginShape();
-    for (const vec of vectors) {
-      // p.curveVertex(vec.x * p.random(p.frameCount* 0.01), vec.y * p.random(p.frameCount * 0.01), 0);
-      // p.curveVertex(vec.x + p.random(-p.frameCount * 0.01, p.frameCount * 0.01), vec.y, 0);
+    for (const vec of lorenz) {
       p.curveVertex(vec.x, vec.y, 0);
-      // p.point(vec.x, vec.y);
     }
     p.endShape();
+
     p.stroke(accentColor);
+    p.beginShape();
+    for (const vec2 of lorenz2) {
+      p.curveVertex(vec2.x, vec2.y, 0);
+    }
+    p.endShape();
+
     p.strokeWeight(0.2);
     p.fill(backgroundColor);
-    p.circle(x, y, 2);
 
     // console.log(x, y);
     // console.log(vectors);
