7ca3d521773c5b5402627c024f7c28fcab1f1240
Gaiwan1 <normalny_uzytkownik@proton.me>
2024-12-16 09:31:52 +0100

    feat: change fonts to ibm plex
    Signed-off-by: Gaiwan1 <normalny_uzytkownik@proton.me>

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   plugins: [
     // viteSingleFile()
     webfontDownload([
-      "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+      "https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap",
     ]),
   ],
 });
